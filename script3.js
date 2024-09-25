7a70a5267f7a020452edddc5e19409b613c0a346
Sanjay Yadav <sjydv03@gmail.com>
2024-09-25 21:28:26 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var menulist = document.getElementById("hammenu");
+    if (menulist.style.display === "block") {
+      menulist.style.display = "none";
+    } else {
+      menulist.style.display = "block";
+    }
+  }
